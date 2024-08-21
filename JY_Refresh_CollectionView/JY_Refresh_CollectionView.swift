763b4492c57d1fbcd25f9cdf2bfe6445ddd12c96
JYYQLin <92315746+JYYQLin@users.noreply.github.com>
2024-08-21 17:59:50 +0800

    Update JY_Refresh_CollectionView.swift

--- JY_Refresh_CollectionView/JY_Refresh_CollectionView.swift
+++ JY_Refresh_CollectionView/JY_Refresh_CollectionView.swift
@@ -23,7 +23,7 @@ open class JY_Refresh_CollectionView: UICollectionView {
         automaticallyAdjustsScrollIndicatorInsets = false
     }
     
-    required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    required public init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented") }
     
 }
 
